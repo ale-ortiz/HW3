42a9aa6c988906e07a330ec31cf7de4686d54ea9
Alejandro Murguia-Ortiz <alejandro-ortiz@uiowa.edu>
2015-10-16 13:55:29 -0500

    added model, dbConn.properties, and ojbc6.jar driver

---
+++ src/java/model/Albums.java
@@ -0,0 +1,73 @@
+package model;
+
+
+public class Albums {
+    
+    private int albumID;
+    private String albumName;
+    private String artistName;
+    private int yearOfRelease;
+    private String genre;
+
+    public Albums() {
+        this.albumID = 0;
+        this.albumName = "";
+        this.artistName = "";
+        this.yearOfRelease = 0;
+        this.genre = "";
+    }
+
+    public Albums(int albumID, String albumName, String artistName, int yearOfRelease, String genre) {
+        this.albumID = albumID;
+        this.albumName = albumName;
+        this.artistName = artistName;
+        this.yearOfRelease = yearOfRelease;
+        this.genre = genre;
+    }
+    
+     public int getAlbumID() {
+        return albumID;
+    }
+
+    public void setAlbumID(int albumID) {
+        this.albumID = albumID;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+       @Override
+    public String toString() {
+        return "Albums{" + "albumID=" + albumID + ", albumName=" + albumName + ", artistName=" + artistName + ", yearOfRelease=" + yearOfRelease + ", genre=" + genre + '}';
+    }
+    
+}
---
+++ web/index.jsp
@@ -0,0 +1,13 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>My Favorite Albums</title>
+    </head>
+    <body>
+        <h1>Database of My Favorite Albums</h1>
+        
+        <a href="read'>View My Favorite Albums</a>
+    </body>
+</html>
